691b26cce68c8ca895ec5e0e06850c0b93f766e1
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-10-30 11:57:19 +0100

    created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+const Header = () => {
+    return (
+        <header className="h-[100px] bg-gray-950 flex px-6 items-center justify-between" >
+
+            {/* Titolo */}
+            <div>
+                <h1 className="text-5xl uppercase font-bold text-red-600" >MovieFlix</h1>
+            </div>
+
+            {/* Filtri */}
+            <div>
+                <select
+                    className="w-56 px-3 text-base mr-4 rounded-md border-0 h-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-600"
+                    name="genre"
+                    id="genre">
+                    <option className="hover:bg-red-600 text-base active:bg-red-600" value="All">All</option>
+                    <option className="hover:bg-red-600 text-base active:bg-red-600" value="Pippo">Pippo</option>
+                    <option className="hover:bg-red-600 text-base active:bg-red-600" value="Giulio">Giulio</option>
+                </select>
+
+                <input className="w-80 placeholder:text-bold placeholder:text-base text-bold text-base placeholder:text-gray-600 px-3 rounded-md border-0 h-10 text-gray-900 shadow-sm ring-1 ring-inset focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-600" type="text" placeholder="Cerca un film o una serie TV" />
+
+            </div>
+
+        </header>
+    )
+}
+
+export default Header;
