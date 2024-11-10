2ffcb74b3b8d8857e48102b286292f4264cd2fea
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-11-10 18:08:42 +0100

    fixed carousel card

--- src/components/carousel/CarouselCard.jsx
+++ src/components/carousel/CarouselCard.jsx
@@ -11,7 +11,7 @@ const CarouselCard = ({ movie }) => {
     return (
         <div className="m-2 w-[250px] h-[200px] hover:scale-[2] transition-all duration-250 ease-in-out flex-shrink-0 cursor-pointer">
 
-            <div className="rounded-3xl overflow-hidden h-full w-full ">
+            <div className="rounded-xl overflow-hidden h-full w-full ">
                 <img className="h-full w-full" src={`${coverSrc()}`} alt={movie.title} />
             </div>
 
