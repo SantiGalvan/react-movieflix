1c26a5cb7a4c69db3c2d32c1b6876a83d788857d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-11-10 16:35:35 +0100

    added genres list into select

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const apiKey = 'api_key=8bc2cb9796f692eef04d762a324418a9';
 const App = () => {
 
   const [ratedMovies, setRatedMovies] = useState();
+  const [genresList, setGenresList] = useState();
 
   const fetchRatedMovies = async () => {
     const res = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?${apiKey}&language=it-IT`);
@@ -15,13 +16,21 @@ const App = () => {
     setRatedMovies(movies);
   }
 
+  const fetchGenresList = async () => {
+    const res = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?${apiKey}&language=it-IT`);
+    const genres = res.data.genres;
+    setGenresList(genres);
+    console.log(genres);
+  }
+
   useEffect(() => {
     fetchRatedMovies();
+    fetchGenresList();
   }, [])
 
   return (
     <>
-      <Header />
+      <Header genres={genresList} />
       <main>
 
         {/* Carosello dei migliori film */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-const Header = () => {
+const Header = ({ genres }) => {
     return (
         <header className="h-[100px] bg-gray-950 flex px-6 items-center justify-between" >
 
@@ -13,9 +13,13 @@ const Header = () => {
                     className="w-56 px-3 text-base mr-4 rounded-md border-0 h-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-600"
                     name="genre"
                     id="genre">
+
                     <option className="hover:bg-red-600 text-base active:bg-red-600" value="All">All</option>
-                    <option className="hover:bg-red-600 text-base active:bg-red-600" value="Pippo">Pippo</option>
-                    <option className="hover:bg-red-600 text-base active:bg-red-600" value="Giulio">Giulio</option>
+
+                    {genres?.map(genre => (
+                        <option className="hover:bg-red-600 text-base active:bg-red-600" value={genre.id}>{genre.name}</option>
+                    ))}
+
                 </select>
 
                 <input className="w-80 placeholder:text-bold placeholder:text-base text-bold text-base placeholder:text-gray-600 px-3 rounded-md border-0 h-10 text-gray-900 shadow-sm ring-1 ring-inset focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-600" type="text" placeholder="Cerca un film o una serie TV" />
